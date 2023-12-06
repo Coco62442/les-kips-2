f5bb965bc0e1e6ba4554ad647001f091be8642c6
coco62442 <corentinclement10@gmail.com>
2023-12-06 21:51:53 +0100

    prémice router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import ErrorPage from './components/Error-page'
+import About from './components/About'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <About />,
+    errorElement: <ErrorPage />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
