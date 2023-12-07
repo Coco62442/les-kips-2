88f20ef4c0544a19ff257809ef5e20b51e530acd
Kylian_Thezenas <kylian.thezenas@etu.umontpellier.fr>
2023-12-07 23:35:56 +0100

    home page avec header et footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,134 @@
-import React from 'react';
-import '../styles/Header.css';  // Assurez-vous de créer ce fichier CSS dans le répertoire src/Styles/
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
+const pages = ['Home', 'Quiz', 'Ressources'];
 
-const Header = () => {
-  return (
-    <header className="header">
-      <div className="logo">Votre Logo</div>
-      <nav>
-        <ul>
-          <li><a href="/">Accueil</a></li>
-          <li><a href="/a-propos">À Propos</a></li>
-          <li><a href="/campagnes">Campagnes en Cours</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+  
+    const handleOpenNavMenu = (event) => {
+      setAnchorElNav(event.currentTarget);
+    };
+  
+    const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+    };
+    
+    return (
+      
+        <AppBar position="static" sx={{ backgroundColor: '#2ecc71' }}>
+          <Container maxWidth="xl" sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: 'flex',
+                  alignItems: 'center',
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'white',
+                  textDecoration: 'none',
+                  '&:hover': {
+                    color: 'red', // Changement de couleur lorsqu'il est survolé
+                  },
+                }}
+              >
+                🎄 LOGO 🌍
+              </Typography>
+    
+              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center" component="a" href="/" sx={{ color: 'inherit', textDecoration: 'none', '&:hover': { color: 'red' } }}>
+                        {page}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+    
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href='/'
+                sx={{
+                  mr: 2,
+                  display: { xs: 'flex', md: 'none' },
+                  flexGrow: 1,
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'white',
+                  textDecoration: 'none',
+                  '&:hover': {
+                    color: 'red', // Changement de couleur lorsqu'il est survolé
+                  },
+                }}
+              >
+                🎄 LOGO 🌍
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    component="a"
+                    href="/"
+                    onClick={handleCloseNavMenu}
+                    sx={{ mx: 2, color: 'white', '&:hover': { color: 'red' } }}
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      );
+    
+  }
+            
 
-export default Header;
+export default ResponsiveAppBar;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom'
 import ErrorPage from './components/Error-page'
 import About from './components/About'
+import Home from './components/Home'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <About />,
+    element: <Home />,
     errorElement: <ErrorPage />,
   }
 ])
